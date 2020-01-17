87e1529286eb26a49cc27f0b5348c20720b0bf6f
lakovicj <31940189+lakovicj@users.noreply.github.com>
2020-01-17 16:23:20 +0100

    services

--- xscience/src/main/java/ftn/xscience/service/CoverLetterService.java
+++ xscience/src/main/java/ftn/xscience/service/CoverLetterService.java
@@ -1,5 +1,24 @@
 package ftn.xscience.service;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ftn.xscience.repository.CoverLetterRepository;
+import ftn.xscience.utils.dom.DOMParser;
+
+@Service
 public class CoverLetterService {
 
+	@Autowired
+	ServletContext context;
+	
+	@Autowired
+	CoverLetterRepository coverLetterRepository;
+	
+	@Autowired
+	DOMParser domParser;
+	
+	private static String schemaLocation = "WEB-INF/classes/data/xsd/coverLetter.xsd";
 }
--- xscience/src/main/java/ftn/xscience/service/ReviewService.java
+++ xscience/src/main/java/ftn/xscience/service/ReviewService.java
@@ -1,5 +1,24 @@
 package ftn.xscience.service;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ftn.xscience.repository.ReviewRepository;
+import ftn.xscience.utils.dom.DOMParser;
+
+@Service
 public class ReviewService {
 
+	@Autowired
+	ServletContext context;
+	
+	@Autowired
+	ReviewRepository reviewRepository;
+	
+	@Autowired
+	DOMParser domParser;
+	
+	private static String schemaLocation = "WEB-INF/classes/data/xsd/review.xsd";
 }
